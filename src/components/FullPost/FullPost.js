3ef094c0e6d42dbf23418efa8563ff1b40e70c43
Daniel Fagonde <cdfagonde@hotmail.com>
2020-01-25 21:52:04 -0300

    Lecture 203 done

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -12,13 +12,21 @@ class FullPost extends Component {
     componentDidUpdate() {
         if(this.props.id) {
             if( !this.state.loadedPost || (this.state.loadedPost.id !== this.props.id)) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+                axios.get('posts/'+this.props.id)
                     .then((response) => {
                         // console.log(response);
                         this.setState({ loadedPost: response.data });
                     });
             }
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('posts/'+this.props.id)
+            .then( response => {
+                console.log("Deleted post "+this.props.id);
+                console.log(response);
+            });
     }
 
     render () {
@@ -32,7 +40,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
     
--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Algumas definições globais de axios
+import axios from 'axios';
+
+// Alguns valores defaul para o axios
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use( req => {
+    console.log(req);
+    // Aqui podemos alterar o request
+    return req;
+}, err => {
+    console.log("Erro interceptado no request!");
+    console.log(err);
+    return Promise.reject(err);
+});
+
+axios.interceptors.response.use( res => {
+    console.log(res);
+    // Aqui podemos alterar response??
+    return res;
+}, err => {
+    console.log("Erro interceptado no response!");
+    console.log(err);
+    return Promise.reject(err);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
